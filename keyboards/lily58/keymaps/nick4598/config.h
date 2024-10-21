7803b83e1bb0fc5ea7ce7c61cdc9ee2bce8d6433
nick4598 <22119573+nick4598@users.noreply.github.com>
2024-10-20 23:11:24 -0400

    working after I did the flash both halves thing

--- keyboards/lily58/keymaps/nick4598/config.h
+++ keyboards/lily58/keymaps/nick4598/config.h
@@ -20,8 +20,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
-#define MASTER_LEFT
-
 // Configure the global tapping term (default: 200ms)
 #define TAPPING_TERM 200
 
